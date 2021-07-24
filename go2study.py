f667581b88145ea6be253fa8d2a46c7244de7f01
sherylynn <352281674@qq.com>
2021-07-25 00:20:19 +0800

    add pdf reader more time & random alert message

--- go2study.py
+++ go2study.py
@@ -1,10 +1,16 @@
 time_count=0
-time_space=90
+time_space=9
+time_pdf=18
 block_title="微信firefox"
 allow_title="通关宝典考试软件选择命令提示符"
 allow_pdf_title="福昕"
 test_title="Sublime"
-alert_message="还在玩,快学习,快起床,快想想你要开玛莎拉蒂,"
+alert_message_list=["还在玩,快学习,快起床,快想想你要开玛莎拉蒂,",
+"我们有时候选择去做一件事情,不是因为它有多简单,而是因为它很难,我们突破自己,挑战自己,变得更强",
+"可能刚刚你走神了,可能刚刚你在忙其他事,不要紧,现在这一分钟就很重要,来做题吧,现在开始就不会迟",
+"想一想,还有人是仰望着你,朝着你走过的路前进,你当做好表率,继续当好榜样,引领别人前行",
+"手机很好玩,微博很好刷,与别人聊天也很有趣,但是你是审计从业者,如果你对注册会计师的证,都觉得无趣,那你这样做,应该吗",
+"有时候需要放松,有时候需要调整,你休息好了吗,现在开始,继续前进前进"]
 #pip3 install pynput==1.6.8
 
 from pynput import mouse
@@ -16,6 +22,8 @@ import win32api
 import win32con
 import win32gui as w
 from datetime import date,timedelta
+
+import random
 
 import pyttsx3 
 # 模块初始化
@@ -49,7 +57,8 @@ def sound_max():
         volume.SetMute(0,None)
     volume.SetMasterVolumeLevel(0.0, None)
 
-
+def alert_message():
+    return alert_message_list[random.randint(0,len(alert_message_list)-1)]
 
 
 def killexe(exe):
@@ -67,7 +76,7 @@ def alert():
     #win32api.MessageBox(None,"快去看书","要命啦!!!!!!!!!!!!!!!!!!!!!",win32con.MB_OK)
     engine = pyttsx3.init() 
     # 设置要播报的Unicode字符串
-    engine.say(alert_message+"离考试倒数天数只有%d天了"%remain_daytime()) 
+    engine.say(alert_message()+"离考试倒数天数只有%d天了"%remain_daytime()) 
     # 等待语音播报完毕 
     engine.runAndWait()
 
@@ -76,10 +85,19 @@ def alert():
 def countTime(inc):
     global time_count
     global time_space
+    global time_pdf
     time_count+=1
+    
     #killexe("按键精灵2014.exe")
     print(time_count)
-    if time_count >= time_space:
+
+    title = w.GetWindowText (w.GetForegroundWindow())
+
+    time_deadline=time_space
+    if allow_pdf_title in title or test_title in title:
+        time_deadline=time_pdf
+
+    if time_count >= time_deadline:
         alert()
     t = Timer(inc, countTime, (inc,))
     t.start()
